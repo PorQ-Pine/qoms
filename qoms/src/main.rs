5a052c3575058f36c492c59945d33437a5e1b4ce
Szybet <53944559+Szybet@users.noreply.github.com>
2025-09-27 22:33:27 +0000

    clean, init, tokio

--- qoms/src/main.rs
+++ qoms/src/main.rs
@@ -1,33 +1,10 @@
-use input::{Libinput, LibinputInterface};
-use libc::{O_RDONLY, O_RDWR, O_WRONLY};
-use std::fs::{File, OpenOptions};
-use std::os::unix::{fs::OpenOptionsExt, io::OwnedFd};
-use std::path::Path;
+pub mod prelude;
+pub mod threads;
 
-struct Interface;
+use crate::prelude::*;
 
-impl LibinputInterface for Interface {
-    fn open_restricted(&mut self, path: &Path, flags: i32) -> Result<OwnedFd, i32> {
-        OpenOptions::new()
-            .custom_flags(flags)
-            .read((flags & O_RDONLY != 0) | (flags & O_RDWR != 0))
-            .write((flags & O_WRONLY != 0) | (flags & O_RDWR != 0))
-            .open(path)
-            .map(|file| file.into())
-            .map_err(|err| err.raw_os_error().unwrap())
-    }
-    fn close_restricted(&mut self, fd: OwnedFd) {
-        drop(File::from(fd));
-    }
-}
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn Error>> {
 
-fn main() {
-    let mut input = Libinput::new_with_udev(Interface);
-    input.udev_assign_seat("seat0").unwrap();
-    loop {
-        input.dispatch().unwrap();
-        for event in &mut input {
-            println!("Got event: {:?}", event);
-        }
-    }
+    Ok(())
 }
