a840276e599cd72b9e7a8a59060a2c5e76136e06
Szybet <53944559+Szybet@users.noreply.github.com>
2025-09-29 18:24:52 +0000

    works

--- qoms/src/threads/qinit.rs
+++ qoms/src/threads/qinit.rs
@@ -46,70 +46,74 @@ impl QinitThread {
         }
     }
 
-async fn main_loop(self) {
-    info!("Qinit main loop entered");
-    loop {
-        info!("Qinit main loop loop");
-        let mut stream = UnixStream::connect(QINIT_SOCKET_PATH).await.unwrap();
-        info!("Connected to QINIT socket");
-        stream
-            .write_all(
-                &postcard::to_allocvec::<CommandToQinit>(&CommandToQinit::GetLoginCredentials)
-                    .unwrap(),
-            )
-            .await
-            .unwrap();
-        stream.shutdown().await.unwrap();
-        info!("Sent GetLoginCredentials command and shut down write");
-
-        let mut buf = [0u8; 512];
-        let mut message_bytes = Vec::new();
-        let mut attempts = 0;
-
+    async fn main_loop(self) {
+        info!("Qinit main loop entered");
         loop {
-            let n = stream.read(&mut buf).await.unwrap();
-            info!("Read attempt {} from QINIT", attempts + 1);
-            if n > 0 {
-                message_bytes.extend_from_slice(&buf[..n]);
-                info!("Received data from QINIT");
-                break;
-            } else {
-                attempts += 1;
-                if attempts >= 5 {
-                    info!("Max read attempts reached");
+            // info!("Qinit main loop loop");
+            let mut stream = UnixStream::connect(QINIT_SOCKET_PATH).await.unwrap();
+            // info!("Connected to QINIT socket");
+            stream
+                .write_all(
+                    &postcard::to_allocvec::<CommandToQinit>(&CommandToQinit::GetLoginCredentials)
+                        .unwrap(),
+                )
+                .await
+                .unwrap();
+            stream.shutdown().await.unwrap();
+            // info!("Sent GetLoginCredentials command and shut down write");
+
+            let mut buf = [0u8; 512];
+            let mut message_bytes = Vec::new();
+            let mut attempts = 0;
+
+            loop {
+                let n = stream.read(&mut buf).await.unwrap();
+                // info!("Read attempt {} from QINIT", attempts + 1);
+                if n > 0 {
+                    message_bytes.extend_from_slice(&buf[..n]);
+                    // info!("Received data from QINIT");
                     break;
+                } else {
+                    attempts += 1;
+                    if attempts >= 5 {
+                        // info!("Max read attempts reached");
+                        break;
+                    }
+                    sleep(Duration::from_millis(50)).await;
+                    // info!("Retrying read");
                 }
-                sleep(Duration::from_millis(50)).await;
-                info!("Retrying read");
             }
-        }
 
-        if message_bytes.is_empty() {
-            info!("No message received, restarting loop");
-            continue;
-        } else {
-            info!("Received data: {:?}", message_bytes);
-        }
+            if message_bytes.is_empty() {
+                // info!("No message received, restarting loop");
+                continue;
+            } else {
+                // info!("Received data: {:?}", message_bytes);
+            }
 
-        match postcard::from_bytes::<libquillcom::socket::AnswerFromQinit>(&message_bytes).unwrap() {
-            libquillcom::socket::AnswerFromQinit::Login(login_form) => match login_form {
-                Some(credentials) => {
-                    debug!("Received credentials from qinit: {:?}", credentials);
-                    self.greetd_sender
-                        .send(MessageToGreetd::LogIn(
-                            credentials.username,
-                            credentials.password,
-                        ))
-                        .await
-                        .unwrap();
-                    info!("Sent login credentials to greetd");
-                    break;
-                }
-                None => info!("Login form was None"),
-            },
+            match postcard::from_bytes::<libquillcom::socket::AnswerFromQinit>(&message_bytes)
+                .unwrap()
+            {
+                libquillcom::socket::AnswerFromQinit::Login(login_form) => match login_form {
+                    Some(credentials) => {
+                        debug!("Received credentials from qinit: {:?}", credentials);
+                        self.greetd_sender
+                            .send(MessageToGreetd::LogIn(
+                                credentials.username,
+                                credentials.password,
+                            ))
+                            .await
+                            .unwrap();
+                        info!("Sent login credentials to greetd");
+                        break;
+                    }
+                    None => {
+                        //info!("Login form was None")
+                    }
+                },
+            }
+            sleep(Duration::from_millis(200)).await;
+            // info!("Loop iteration complete");
         }
-        sleep(Duration::from_millis(200)).await;
-        info!("Loop iteration complete");
     }
-}
 }
